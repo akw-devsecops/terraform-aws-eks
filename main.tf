b213053cf22db6bbef2dd077cf683426fcfddd47
Janosch Schäfer <janosch.schaefer@de.kaercher.com>
2023-06-29 10:42:41 +0200

    reorder modules

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-module "aws_load_balancer_controller" {
-  source = "./modules/aws-load-balancer-controller"
-
-  enable_aws_load_balancer_controller = var.enable_aws_load_balancer_controller
-
-  cluster_name      = module.eks.cluster_name
-  oidc_provider_arn = module.eks.oidc_provider_arn
-}
-
-module "cert_manager" {
-  source = "./modules/cert-manager"
-
-  enable_cert_manager = var.enable_cert_manager
-
-  enable_cluster_issuer_letsencrypt = var.enable_cluster_issuer_letsencrypt
-  cluster_issuer_letsencrypt_email  = var.cluster_issuer_letsencrypt_email
-}
-
 module "aws_ebs_csi_driver" {
   source = "./modules/aws-ebs-csi-driver"
 
@@ -34,8 +16,26 @@ module "aws_efs_csi_driver" {
   oidc_provider_arn = module.eks.oidc_provider_arn
 }
 
+module "aws_load_balancer_controller" {
+  source = "./modules/aws-load-balancer-controller"
+
+  enable_aws_load_balancer_controller = var.enable_aws_load_balancer_controller
+
+  cluster_name      = module.eks.cluster_name
+  oidc_provider_arn = module.eks.oidc_provider_arn
+}
+
 module "calico" {
   source = "./modules/calico"
 
   enable_calico = var.enable_calico
+}
+
+module "cert_manager" {
+  source = "./modules/cert-manager"
+
+  enable_cert_manager = var.enable_cert_manager
+
+  enable_cluster_issuer_letsencrypt = var.enable_cluster_issuer_letsencrypt
+  cluster_issuer_letsencrypt_email  = var.cluster_issuer_letsencrypt_email
 }
