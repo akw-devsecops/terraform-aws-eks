8c14cb8fe270ccfdc75a8f6149f638ec68129bee
Janosch Schäfer <janosch.schaefer@de.kaercher.com>
2023-07-18 09:59:58 +0200

    add option to pass additional dns zones

--- main.tf
+++ main.tf
@@ -9,6 +9,25 @@ locals {
     AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG = "true"
     ENI_CONFIG_LABEL_DEF               = "topology.kubernetes.io/zone"
   } : {}
+
+  corefile = <<EOF
+.:53 {
+    errors
+    health
+    kubernetes cluster.local in-addr.arpa ip6.arpa {
+      pods insecure
+      fallthrough in-addr.arpa ip6.arpa
+    }
+    prometheus :9153
+    forward . /etc/resolv.conf
+    cache 30
+    loop
+    reload
+    loadbalance
+}
+${var.coredns_additional_zones}
+EOF
+
 }
 
 module "eks" {
@@ -34,22 +53,7 @@ module "eks" {
     coredns = {
       most_recent = true
       configuration_values = jsonencode({
-        corefile = <<EOF
-.:53 {
-    errors
-    health
-    kubernetes cluster.local in-addr.arpa ip6.arpa {
-      pods insecure
-      fallthrough in-addr.arpa ip6.arpa
-    }
-    prometheus :9153
-    forward . /etc/resolv.conf
-    cache 30
-    loop
-    reload
-    loadbalance
-}
-EOF
+        corefile = local.corefile
       })
     }
   }
