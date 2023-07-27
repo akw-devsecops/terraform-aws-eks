7749ac796ac34c508df4cc7fd8daf49090cf2bc4
Janosch Schäfer <janosch.schaefer@de.kaercher.com>
2023-07-27 14:28:31 +0200

    Update cluster autoscaler version to v1.25.3

--- modules/cluster-autoscaler/main.tf
+++ modules/cluster-autoscaler/main.tf
@@ -19,7 +19,7 @@ module "cluster_autoscaler_irsa_role" {
 
 locals {
   cluster_autoscaler_version = {
-    "1.25" = "v1.25.2"
+    "1.25" = "v1.25.3"
     "1.26" = "v1.26.3"
     "1.27" = "v1.27.2"
   }
