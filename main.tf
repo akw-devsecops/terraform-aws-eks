6ec1e014c8d5337b836f950aa9b5549b15b2e485
Janosch Schäfer <janosch.schaefer@karcher.com>
2024-07-18 16:41:12 +0200

    Can't map all aws-auth groups to access entry groups

--- main.tf
+++ main.tf
@@ -87,20 +87,6 @@ EOF
       }
     }
   } : {}
-
-  additional_roles = {
-    for role in var.iam_additional_roles : role.username => {
-      principal_arn     = role.userarn
-      kubernetes_groups = role.groups
-    }
-  }
-
-  additional_users = {
-    for user in var.iam_additional_users : user.username => {
-      principal_arn     = user.userarn
-      kubernetes_groups = user.groups
-    }
-  }
 }
 
 module "eks" {
@@ -192,9 +178,19 @@ module "eks" {
     local.admin_role,
     local.argo_cd_cluster_management,
     local.argo_cd_application_management,
-    local.additional_roles,
-    local.additional_users,
   )
+}
+
+module "aws_auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "~> 20.0"
+
+  manage_aws_auth_configmap = true
+  aws_auth_roles = concat(
+    var.iam_additional_roles
+  )
+
+  aws_auth_users = var.iam_additional_users
 }
 
 locals {
