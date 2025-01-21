34c5aa50e261deadaeda4e059168d91c7014e728
Janosch Schäfer <janosch.schaefer@karcher.com>
2025-01-21 14:45:34 +0100

    Disable cluster logging by default

--- variables.tf
+++ variables.tf
@@ -87,6 +87,12 @@ variable "iam_argo_cd_application_management_role" {
   default     = null
 }
 
+variable "cluster_enabled_log_types" {
+  description = "A list of the desired control plane logs to enable. For more information, see Amazon EKS Control Plane Logging documentation (https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html)"
+  type        = list(string)
+  default     = []
+}
+
 ################################################################################
 # Addons
 ################################################################################
